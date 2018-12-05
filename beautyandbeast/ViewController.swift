10396899dd09c6032c63694bfedde762a735c167
sashachiu <sashachiu@sashachiude-macbook-pro.local>
2018-12-05 23:00:56 +0800

    Initial Commit

---
+++ beautyandbeast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  beautyandbeast
+//
+//  Created by sashachiu on 2018/12/5.
+//  Copyright © 2018 sashachiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
