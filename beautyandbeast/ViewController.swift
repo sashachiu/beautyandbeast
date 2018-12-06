634d8be5d5254e3d25a7f9488ec79bbbbbe542da
“Your <sashachiu456@gmail.com>
2018-12-06 13:03:45 +0800

    v2

--- beautyandbeast/ViewController.swift
+++ beautyandbeast/ViewController.swift
@@ -10,10 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var belleImageView: UIImageView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var alphaSlider: UISlider!
+    
+    @IBOutlet weak var beautybelleImageView: UIImageView!
+    @IBOutlet weak var beautybeastImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+
+    @IBAction func redSliderControl(_ sender: Any) {
+        belleImageView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
+    }
+    
+    @IBAction func greenSliderControl(_ sender: Any) {
+    belleImageView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
+    }
+    
+    @IBAction func blueSliderControl(_ sender: Any) {
+    belleImageView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
+        
+    }
+    
+    @IBAction func alphaSliderControl(_ sender: Any) {
+    belleImageView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
+    }
+
+
+    @IBAction func closerSliderControl(_ sender: UISlider) {
+        beautybelleImageView.alpha = CGFloat(sender.value)
+        beautybelleImageView.frame.origin.x = CGFloat(10+188*sender.value)
+        beautybeastImageView.frame.origin.x = CGFloat(10-188*sender.value)
+    }
+    
+
 
 
 }
